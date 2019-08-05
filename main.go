1cbb1fac217b0587b118aceaf0a29609fb050aea
zheng <765324639@qq.com>
2019-08-06 00:15:25 +0800

    add gin-gwt

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/zavier/blog-admin-backend/handler"
+	"github.com/zavier/blog-admin-backend/middleware"
 	"log"
 )
 
@@ -18,9 +19,13 @@ func main() {
 	// 登陆相关
 	userRouter := router.Group("/user")
 	userRouter.POST("/register", handler.Register)
-	userRouter.POST("/login", handler.Login)
+	userRouter.POST("/login", middleware.JwtMiddleware().LoginHandler)
+	// todo 可能不需要退出了，让前端自己删除token即可，是否登陆也可以让前段自行判断
 	userRouter.POST("/logout", handler.Logout)
 	userRouter.GET("/isLogin", handler.IsLogIn)
+
+	// 下面的功能开始需要进行认证了
+	router.Use(middleware.JwtMiddleware().MiddlewareFunc())
 
 	// 博客相关
 	blogRouter := router.Group("/blog")
